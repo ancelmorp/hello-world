b7f05cff0dbe92ee54bfa2358599443d6c756f91
ancelmorp <ancelmo.rodriguez@gmail.com>
2021-06-01 08:12:13 -0500

    Create HolaMundo.java

---
+++ HolaMundo.java
@@ -0,0 +1,11 @@
+public class HolaMundoApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hola Mundo\n");
+    }
+    
+}
