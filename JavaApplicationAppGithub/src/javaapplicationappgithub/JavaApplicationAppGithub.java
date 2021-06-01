fa05452d793daf863206799bc6091ae6f2ddd465
ancelmorp <ancelmo.rodriguez@gmail.com>
2021-06-01 08:31:25 -0500

    Update JavaApplicationAppGithub.java

--- JavaApplicationAppGithub/src/javaapplicationappgithub/JavaApplicationAppGithub.java
+++ JavaApplicationAppGithub/src/javaapplicationappgithub/JavaApplicationAppGithub.java
@@ -15,7 +15,7 @@ public class JavaApplicationAppGithub {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hola Mundo desde escritorio\n");
+        System.out.println("Hola Mundo desde escritorio editado en línea\n");
     }
     
 }
