3369d76c533247f2893ff77ce979ab53a79b3506
ancelmorp <ancelmo.rodriguez@gmail.com>
2021-06-01 08:14:36 -0500

    Update HolaMundo.java
    Se eliminaron los comentarios por defecto

--- HolaMundo.java
+++ HolaMundo.java
@@ -1,10 +1,8 @@
 public class HolaMundoApp {
-
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
         System.out.println("Hola Mundo\n");
     }
     
